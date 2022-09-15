d369911ceb644b22de7fee4a7a2b4d00ce38af50
Natalie Wagner <nrwagner723@gmail.com>
2022-09-15 14:50:05 -0500

    started working on changing the home page and adding pages to navbar

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -2,7 +2,6 @@ import React from "react";
 import { useEffect, useState } from "react";
 import useAuth from "../../hooks/useAuth";
 import { Link } from "react-router-dom";
-
 import axios from "axios";
 
 const HomePage = () => {
